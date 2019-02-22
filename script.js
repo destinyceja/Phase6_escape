7e334f0b21e794dc1246e4a1cfd6f739acb5b189
destinyceja <33135802+destinyceja@users.noreply.github.com>
2019-02-22 15:42:57 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+/* alice-alice2-alice3-scene4*/
+function nextImage(el){
+	if (el.src.match("images/alice.PNG")){
+	  el.src = "images/alice2.PNG";
+	} else if (el.src.match("images/alice2.PNG")){
+		el.src = "images/alice3.PNG";
+	} else if (el.src.match("images/alice3.PNG")){
+		el.src = "images/scene4.JPG";
+	} else {
+		el.src = "images/alice.PNG";
+	}
+}
+/* alice3-scene4-alice-alice2*/
+function nextImage2(el){
+	if (el.src.match("images/alice3.PNG")){
+	  el.src = "images/scene4.JPG";
+	} else if (el.src.match("images/scene4.JPG")){
+		el.src = "images/alice.PNG";
+	} else if (el.src.match("images/alice.PNG")){
+		el.src = "images/alice2.PNG";
+	} else {
+		el.src = "images/alice3.PNG";
+	}
+}
+/* alice2-alice3-scene4-alice*/
+function nextImage3(el){
+	if (el.src.match("images/alice2.PNG")){
+	  el.src = "images/alice3.PNG";
+	} else if (el.src.match("images/alice3.PNG")){
+		el.src = "images/scene4.JPG";
+	} else if (el.src.match("images/scene4.JPG")){
+		el.src = "images/alice.PNG";
+	} else {
+		el.src = "images/alice2.PNG";
+	}
+}
+/* alice3-scene4-alice-alice2*/
+function nextImage4(el){
+	if (el.src.match("images/alice3.PNG")){
+	  el.src = "images/scene4.JPG";
+	} else if (el.src.match("images/scene4.JPG")){
+		el.src = "images/alice.PNG";
+	} else if (el.src.match("images/alice.PNG")){
+		el.src = "images/alice2.PNG";
+	} else {
+		el.src = "images/alice3.PNG";
+	}
+}
+/* alice2-alice3-alice-scene4*/
+function nextImage5(el){
+	if (el.src.match("images/alice2.PNG")){
+	  el.src = "images/alice3.PNG";
+	} else if (el.src.match("images/alice3.PNG")){
+		el.src = "images/alice.PNG";
+	} else if (el.src.match("images/alice.PNG")){
+		el.src = "images/scene4.JPG";
+	} else {
+		el.src = "images/alice2.PNG";
+	}
+}
+/* scene4-alice-alice2-alice3*/
+function nextImage6(el){
+	if (el.src.match("images/scene4.JPG")){
+	  el.src = "images/alice.PNG";
+	} else if (el.src.match("images/alice.PNG")){
+		el.src = "images/alice2.PNG";
+	} else if (el.src.match("images/alice2.PNG")){
+		el.src = "images/alice3.PNG";
+	} else {
+		el.src = "images/scene4.JPG";
+	}
+}
